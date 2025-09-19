f195bb9cb34c3a3ba9331ef30d5071c0e4dadfac
huzaifa321s <muhammadhuzaifa@humantekteam.com>
2025-09-19 16:31:02 +0500

    routes added

---
+++ routes/web.js
@@ -0,0 +1,16 @@
+import express from "express"
+import courseController from "../controllers/web/course.js";
+import gamesController from "../controllers/web/game.js";
+import { isAuthenticated, isStudent } from "../middlewares/Auth.js";
+
+const router = express.Router();
+
+// Course
+router.route('/course/get').get(courseController.get);
+router.route('/course/getCourse').get(courseController.getCourse);
+
+// Games
+router.route('/game/training-wheel-game/get').get([isAuthenticated, isStudent], gamesController.getTrainingWheelQuestions);
+
+
+export default router;
