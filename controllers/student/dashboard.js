17d550f95ce63886f6dc36943b464e79663701da
huzaifa321s <muhammadhuzaifa@humantekteam.com>
2025-10-10 12:47:38 +0500

    student dashboard api corrected

--- controllers/student/dashboard.js
+++ controllers/student/dashboard.js
@@ -9,6 +9,7 @@ const stripeInstance = stripe(process.env.STRIPE_SECRET_KEY);
 
 const dashboardController = {
   get: async (req, res) => {
+    console.log('get func')
     try {
       const isSubscriber = checkSubscription(req.user.subscriptionId);
 
